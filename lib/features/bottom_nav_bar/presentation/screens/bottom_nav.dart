1ca594c09a19ab74cba356ade40941d3c83aaf6f
Alaminpk05 <mdalaminpk05@gmail.com>
2024-08-30 11:55:43 +0600

    designed pending and bottom nav bar

--- lib/features/bottom_nav_bar/presentation/screens/bottom_nav.dart
+++ lib/features/bottom_nav_bar/presentation/screens/bottom_nav.dart
@@ -1,10 +1,13 @@
 
-import 'dart:async';
+
+import 'dart:ui';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:sizer/sizer.dart';
 import 'package:stylish_bottom_bar/stylish_bottom_bar.dart';
-import 'package:task_manager_system/features/pending.dart';
+import 'package:task_manager_system/features/completed.dart';
+import 'package:task_manager_system/features/task/presentation/pending.dart';
 import 'package:task_manager_system/features/reminders.dart';
 import 'package:task_manager_system/features/reports.dart';
 
@@ -24,7 +27,7 @@ List<Map<String, dynamic>> pageDetails = [
   },
   {
 
-    'page':  Completer(),
+    'page':  Completed(),
   },
   {
 
@@ -36,12 +39,13 @@ List<Map<String, dynamic>> pageDetails = [
 ];
 
 class _BottomNavState extends State<BottomNav> {
-   MaterialColor selectedColor = Colors.green;
+   Color selectedColor = Color.fromRGBO(80, 203, 222, 1);
    Color unSelectedColor = Color(0xff8C8C8C);
   @override
   Widget build(BuildContext context) {
     return Scaffold (
       body: pageDetails[index]['page'],
+
       bottomNavigationBar:StylishBottomBar(
 
         items: [
@@ -52,7 +56,7 @@ class _BottomNavState extends State<BottomNav> {
             selectedIcon: const Icon(
               Icons.style,
             ),
-            selectedColor: selectedColor,
+            selectedColor:selectedColor,
             unSelectedColor: unSelectedColor,
             title: const Text('Pending'),
           ),
@@ -80,26 +84,150 @@ class _BottomNavState extends State<BottomNav> {
              Icons.bar_chart_outlined,
             ),
             selectedIcon: const Icon(
-              Icons.access_time_filled_outlined,
+              Icons.bar_chart_outlined,
             ),
             selectedColor: selectedColor,
             unSelectedColor: unSelectedColor,
             title: const Text('Reports'),
           ),
         ],
-        hasNotch: false,
+        hasNotch: true,
         fabLocation: StylishBarFabLocation.center,
         currentIndex: index,
-        onTap: (index) {
-
+        onTap: (value) {
+          setState(() {
+            index=value;
+            print(index);
+          });
         },
+
         option: AnimatedBarOptions(
           // iconSize: 32,
           barAnimation: BarAnimation.fade,
           iconStyle: IconStyle.Default,
           // opacity: 0.3,
         ),
-      )
+
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Color.fromRGBO(0, 188, 212,1),
+        shape:  CircleBorder(),
+          child: Icon(Icons.edit,color: Colors.black,),
+          onPressed: (){
+
+            showDialog(
+                context: context,
+                builder: (BuildContext context) {
+                  return AddTaskCard();
+          });
+
+          }),
+
+    );
+  }
+}
+
+class AddTaskCard extends StatelessWidget {
+  const AddTaskCard({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return BackdropFilter(
+      filter: ImageFilter.blur(sigmaX: 6, sigmaY: 6),
+      child: AlertDialog(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+        title: Column(
+          children: [
+            Container(
+              height: 45,
+              width: 45,
+              decoration: const BoxDecoration(
+                shape: BoxShape.circle,
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black26,
+                    blurRadius: 20,
+                    spreadRadius: 0,
+                    offset: Offset(0, 0),
+                  )
+                ],
+                color: Colors.white,
+              ),
+              child: const Icon(
+                Icons.note_alt_outlined,
+                color: Color.fromARGB(255, 143, 128, 128),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 10.0),
+              child: Text(
+                'New task',
+                style: TextStyle(
+                  fontSize: 23,
+                  color: const Color.fromARGB(255, 143, 128, 128),
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ],
+        ),
+        content: SizedBox(
+          height: 9.h,
+          child: TextField(
+
+             maxLines: null,
+            expands: false,
+            style: TextStyle(
+              color: const Color.fromARGB(255, 128, 124, 124),
+            ),
+            keyboardType: TextInputType.multiline,
+            decoration: InputDecoration(
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(18.sp),
+              ),
+              filled: false,
+              hintText: 'What are you planning?',
+              hintStyle:TextStyle(
+                color: const Color(0xff9C9A9A),
+              ),
+            ),
+    // controller: textEditingController,
+            onChanged: (value) {
+
+            },
+            onTapOutside: (event) {
+
+            },
+          ),
+        ),
+        actionsAlignment: MainAxisAlignment.spaceEvenly,
+        actions: <Widget>[
+          TextButton(
+            child: Text(
+              "Cancel",
+              style: TextStyle(
+                fontSize: 15,
+                color: Colors.red,
+              ),
+            ),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+          ),
+          TextButton(
+            onPressed: (){},
+            child: Text(
+              "Create",
+              style: TextStyle(
+                fontSize: 15,
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,42 @@
 import 'package:flutter/material.dart';
-import 'package:task_manager_system/features/bottom_nav_bar/presentation/screens/bottom_nav.dart';
+import 'package:sizer/sizer.dart';
+
+import 'features/bottom_nav_bar/presentation/screens/bottom_nav.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
+    return ResponsiveSizer(
+      builder: (context, orientation, screenType){
+      return MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          floatingActionButtonTheme: FloatingActionButtonThemeData(
+            backgroundColor: Colors.green
+          ),
+          appBarTheme: AppBarTheme(
+            color: Colors.green
+          ),
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: false,
+        ),
+          debugShowCheckedModeBanner: false,
+        home:const BottomNav()
 
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: false,
-      ),
-        debugShowCheckedModeBanner: false,
-      home:BottomNav()
 
-
+      );},
     );
   }
 }
